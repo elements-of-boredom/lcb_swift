b7531bf18df4a4fff3a3d754ffe85c116a63d848
Greg Williams <greg.williams@mckesson.com>
2017-04-26 21:27:04 -0500

    Fix build script, add README

---
+++ README.md
@@ -0,0 +1,4 @@
+#LCB_Swift  
+
+A wrapper around the C library `libcouchbase` for Swift. 
+Will attempt to mirror the Node.js SDK as closely as logically makes sense.
--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-swift build -Xcc -I/usr/local/Cellar/libcouchbase/2.7.4/include -Xlinker -L/usr/local/Cellar/libcouchbase/2.7.4/lib/  -Xcc -I./Modules -v
+swift build -Xcc -I/usr/local/Cellar/libcouchbase/2.7.4/include -Xlinker -L/usr/local/Cellar/libcouchbase/2.7.4/lib/ -Xcc -I./libcouchbase -v
